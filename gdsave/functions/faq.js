a31aae06fc05f1c74eef5d8c445fb1fd7ccd5cbe
ryder7223 <114200735+ryder7223@users.noreply.github.com>
2023-05-08 10:22:10 +0800

    Add files via upload

---
+++ gdsave/functions/faq.js
@@ -0,0 +1,26 @@
+let FAQ = [
+
+    {
+        "i": "huh",
+        "q": "Why do so many levels say \"not saved\"?",
+        "a": "To save space, GD does not store the full info on every level you've ever downloaded. For most levels, only important info like percentage and star rating can be accessed, the latter of which can be used to roughly estimate the level's difficulty."
+    },
+
+    {
+        "i": "demons/unknown",
+        "q": "What are NA demons? Why are there no hard demons?",
+        "a": "Because of how demons are stored in the GD save file, <u>hard demons cannot be displayed accurately</u>. This is because hard demons serve as the 'default' demon rating and 99% of the time they end up with a different rating after an hour or two."
+    },
+
+    {
+        "i": "demons/easy",
+        "q": "Why are demon difficulties sometimes inaccurate?",
+        "a": "Since the community votes on demon difficulties, their rating is constantly changing. Your save file bases the demon difficulty off of the last time you downloaded them, and it is very likely that some ratings changed since then.",
+    },
+
+    {
+        "i": "lock",
+        "q": "How do I know this site won't steal my GD data?",
+        "a": "If you dig around in this site's code you'll find that everything is 100% offline. This means that NOTHING is uploaded to a server, and all the calculations are done by your own web browser. The only exception to this is that levels in the 'all levels' tab have their data fetched from GDBrowser.",
+    }
+]
